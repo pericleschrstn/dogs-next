c2b89f64eee62ae2e0361a08b1cba970ccca92c0
Pericles Christian <pericleschrstn@gmail.com>
2024-03-21 11:08:00 -0300

    feat: add hooks useFormStatus and useFormState

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,22 +1,30 @@
 "use server";
 
+import { TOKEN_POST } from "@/functions/api";
+import ApiError from "@/functions/api-error";
 import { cookies } from "next/headers";
 
-export default async function login(formData: FormData) {
+export default async function login(state: {}, formData: FormData) {
   const username = (formData.get("username") as string) || null;
   const password = (formData.get("password") as string) || null;
 
-  const response = await fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-    method: "POST",
-    body: formData,
-  });
-  const data = await response.json();
-  cookies().set("token", data.token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "lax",
-    maxAge: 60 * 60 * 24,
-  });
-
-  console.log(data);
+  try {
+    if (!username || !password) throw new Error("Preencha os dados");
+    const { url } = TOKEN_POST();
+    const response = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+    if (!response.ok) throw new Error("Usuário ou senha inválidos");
+    const data = await response.json();
+    cookies().set("token", data.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24,
+    });
+    return { data: null, ok: true, error: "" };
+  } catch (error: unknown) {
+    return ApiError(error);
+  }
 }
--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,12 +1,28 @@
-import login from "@/actions/login";
+"use client";
 
-export default async function LoginForm() {
+import login from "@/actions/login";
+import { useFormState, useFormStatus } from "react-dom";
+import Button from "@/components/forms/button";
+
+function ButtonForm() {
+  const { pending } = useFormStatus();
+
+  return <>{pending ? <Button disabled={pending}>Enviando...</Button> : <Button>Entrar</Button>}</>;
+}
+
+export default function LoginForm() {
+  const [state, action] = useFormState(login, {
+    ok: false,
+    error: "",
+    data: null,
+  });
   return (
     <>
-      <form action={login}>
+      <form action={action}>
         <input type="text" placeholder="usuário" name="username" />
         <input type="password" placeholder="senha" name="password" />
-        <button>Entrar</button>
+        <ButtonForm />
+        <p>{state.error}</p>
       </form>
     </>
   );
