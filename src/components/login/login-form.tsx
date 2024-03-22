0992c1df18c3fbffcb86e9072e126bd673848df4
Pericles Christian <pericleschrstn@gmail.com>
2024-03-22 10:19:57 -0300

    feat: add styles login page

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -6,6 +6,8 @@ import Button from "@/components/forms/button";
 import Input from "@/components/forms/input";
 import ErrorMessage from "@/components/helper/error-message";
 import React from "react";
+import style from "@/components/login/login-form.module.css";
+import Link from "next/link";
 
 function ButtonForm() {
   const { pending } = useFormStatus();
@@ -26,12 +28,22 @@ export default function LoginForm() {
 
   return (
     <>
-      <form action={action}>
+      <form action={action} className={style.form}>
         <Input label="Usuário" name="username" type="text" />
         <Input label="Senha" name="password" type="password" />
         <ErrorMessage error={state.error} />
         <ButtonForm />
       </form>
+      <Link href="/login/perdeu" className={style.perdeu}>
+        Perdeu a senha?
+      </Link>
+      <div className={style.cadastro}>
+        <h2 className={style.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link href="/login/criar" className="button">
+          Cadastro
+        </Link>
+      </div>
     </>
   );
 }
