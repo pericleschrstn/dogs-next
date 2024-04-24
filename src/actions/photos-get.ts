77c2a913c140a81be07c6bf888c64566cd493712
Pericles Christian <pericleschrstn@gmail.com>
2024-04-24 10:32:14 -0300

    feat: add feed route conta

--- src/actions/photos-get.ts
+++ src/actions/photos-get.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { PHOTOS_GET } from "@/functions/api";
+import ApiError from "@/functions/api-error";
+
 export type Photo = {
   id: number;
   author: string;
@@ -12,10 +15,22 @@ export type Photo = {
   total_comments: string;
 };
 
-export default async function photosGet() {
-  const response = await fetch("https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0", {
-    next: { revalidate: 10, tags: ["photos"] },
-  });
-  const data = (await response.json()) as Photo[];
-  return data;
+type PhotosGetParams = {
+  page?: number;
+  total?: number;
+  user?: 0 | string;
+};
+
+export default async function photosGet({ page = 1, total = 6, user = 0 }: PhotosGetParams = {}) {
+  try {
+    const { url } = PHOTOS_GET({ page, total, user });
+    const response = await fetch(url, {
+      next: { revalidate: 10, tags: ["photos"] },
+    });
+    if (!response.ok) throw new Error("Falha ao buscar as fotos");
+    const data = (await response.json()) as Photo[];
+    return { data, ok: true, error: "" };
+  } catch (error) {
+    return ApiError(error);
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@ import photosGet from "@/actions/photos-get";
 import Feed from "@/components/feed/feed";
 
 export default async function HomePage() {
-  const data = await photosGet();
+  const { data } = await photosGet();
 
   return (
     <section className="container mainContainer">
-      <Feed photos={data} />
+      {data?.length ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+          <p>Nenhuma foto foi postada</p>
+        </div>
+      )}
     </section>
   );
 }
